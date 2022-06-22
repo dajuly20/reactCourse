e49cdd485ebb0634ce7912b45cf2ae4cbbfd0d7c
Julian Wiche <julian@wiche.eu>
2022-06-22 10:20:22 +0200

    set up all dependencies, trying to switch to remote WSL Plugin

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
+"use strict";
+
 console.log("App.js is running");
 
-var template = React.createElement("p", null, "This is JSX from app.js!");// <p>This is JSX from app.js!</p>
+var template = React.createElement(
+  "p",
+  null,
+  "This is JSX from the NEW app.js!"
+);
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
